d6b83abfd926cb1bc3a71285611a6647b73b51e8
Henrik Boulund Meng Hansen <hbh@easv.dk>
2025-11-25 09:41:07 +0100

    Added info regarding port

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ def get_greeting(name: str) -> str:
     return f"Hello, {name}!"
 
 if __name__ == "__main__":
-    mcp.run(transport="http", port=8000)
+    mcp.run(transport="http", host="127.0.0.1", port=8000)
